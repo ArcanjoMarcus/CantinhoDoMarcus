e8d52c8ad76a7811fedab359600ed84651f0da9b
Marcus <marcusarcanjo02@gmail.com>
2021-05-27 08:12:56 -0300

    Arrumando a sala

---
+++ js/data.js
@@ -0,0 +1,42 @@
+ Amplitude.init({
+    songs: [
+        {
+            "title":"first",
+            "artist":"arcanjo",
+            "cover_art_url":"musicas/covers/white_square.jpg",
+            "album":"Lost and Found",
+            "url":"musicas/first.wav",
+    
+        },
+        {
+            "title":"misterio",
+            "artist":"arcanjo",
+            "cover_art_url":"musicas/covers/white_square.jpg",
+            "album":"Lost and Found",
+            "url":"musicas/misterio.wav"   
+        },
+        {
+            "title":"trip",
+            "artist":"arcanjo",
+            "cover_art_url":"musicas/covers/white_square.png",
+            "album":"Lost and Found",
+            "url":"musicas/trip.wav"
+    
+        },
+        {
+            "title":"grass",
+            "artist":"arcanjo",
+            "cover_art_url":"musicas/covers/white_square.png",
+            "album":"Lost and Found",
+            "url":"musicas/grass.wav"
+        },
+        {
+            "title":"grass_ketchup",
+            "artist":"arcanjo",
+            "cover_art_url":"musicas/covers/white_square.png",
+            "album":"Lost and Found",
+            "url":"musicas/grass_ketchup.wav"
+        },
+    ]
+})
+
